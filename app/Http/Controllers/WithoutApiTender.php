69dc08c3ffcab9a8a316b3e9ca1fb40bd78bbe2c
amazbhola <amazingbhola@gmail.com>
2024-12-07 00:57:48 +0600

    comment change

--- app/Http/Controllers/WithoutApiTender.php
+++ app/Http/Controllers/WithoutApiTender.php
@@ -8,11 +8,11 @@ use Illuminate\Http\Request;
 
 class WithoutApiTender extends Controller
 {
-    function allTender(){
+    function allTender()
+    {
 
-        // $tenders = Tender::where('last_sale_date', '>', date('Y-m-d'))->with( ['location'=>function($query){$query->select('upazilas.id','upazilas.name');}])->get();
-        $tenders = DB::table('tenders')->select('tenders.tender_id', 'tenders.description', 'tenders.document_price', 'tenders.tender_security', 'tenders.method','tenders.last_sale_date', 'tenders.liquid_amount','tenders.similar_works','tenders.turnover', 'tenders.tender_capacity', 'tenders.note',  'upazilas.name as location_name', 'departments.name as department_name')->leftJoin('upazilas', 'upazilas.id', '=', 'tenders.location_id')
-        ->leftJoin('departments', 'departments.id', '=', 'tenders.department_id')->where('last_sale_date', '>', date('Y-m-d'))->get();
+        $tenders = DB::table('tenders')->select('tenders.tender_id', 'tenders.description', 'tenders.document_price', 'tenders.tender_security', 'tenders.method', 'tenders.last_sale_date', 'tenders.liquid_amount', 'tenders.similar_works', 'tenders.turnover', 'tenders.tender_capacity', 'tenders.note',  'upazilas.name as location_name', 'departments.name as department_name')->leftJoin('upazilas', 'upazilas.id', '=', 'tenders.location_id')
+            ->leftJoin('departments', 'departments.id', '=', 'tenders.department_id')->where('last_sale_date', '>', date('Y-m-d'))->get();
         return $tenders;
     }
 }
